d2731dc2fd2fbd19c4f8a6595316d62990f0dcfe
Milana Moroz <milanamoroz@MacBook-Air-Milana.local>
2023-07-27 13:02:20 +0300

    homework

---
+++ first_homework.py
@@ -0,0 +1,16 @@
+
+country = 'Країна в якій я живу \nназивається Україна'
+print(country)
+
+string = 'Mоя асоціація з цією країною-'
+association = '🌻'
+print(string + association)
+
+association2 = '\U0001F33B'
+print(string + association2)
+
+summa = f'{country} - {association2}'
+print(summa)
+
+summa = country + ' - ' + association2
+print(summa)
